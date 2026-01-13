5615dd654eed40aa0759d527146340066165b19f
Salvatore Di Lorenzo <salvadil02@gmail.com>
2026-01-13 16:31:52 +0100

    initial commit

---
+++ assignment_06/main.py
@@ -0,0 +1,396 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from simulation import Simulation
+import pid
+import purepursuit
+import stanley
+from mpc import *
+import cubic_spline_planner
+import math
+import matplotlib
+import frenet_optimal_trajectory as fp 
+
+# Simulation parameters
+dt = 0.05         # Time step (s)
+ax = 0.0            # Constant longitudinal acceleration (m/s^2)
+steer = 0.0      # Constant steering angle (rad)
+sim_time = 75    # Simulation duration in seconds
+steps = int(sim_time / dt)  # Simulation steps
+
+# Control references
+target_speed = 20
+
+# Vehicle parameters
+lf = 1.156          # Distance from COG to front axle (m)
+lr = 1.42           # Distance from COG to rear axle (m)
+wheelbase = lf + lr
+mass = 1200         # Vehicle mass (kg)
+Iz = 1792           # Yaw moment of inertia (kg*m^2)
+max_steer = 3.14  # Maximum steering angle in radians
+
+# Create instance of PID for Longitudinal Control
+long_control_pid = pid.PIDController(kp=1.6, ki=0.85, kd=0.01, output_limits=(-2, 2))
+
+# Create instance of PurePursuit, Stanley and MPC for Lateral Control
+k_pp = 0.1  # Speed proportional gain for Pure Pursuit
+look_ahead = 2.0  # Minimum look-ahead distance for Pure Pursuit
+k_stanley = 1.9  # Gain for cross-track error for Stanley
+pp_controller = purepursuit.PurePursuitController(wheelbase, max_steer)
+stanley_controller = stanley.StanleyController(k_stanley, lf, max_steer)
+
+#Planning
+# obstacle lists
+ob = np.array([[100.0, -0.5],
+                [400.0, 0.5],
+                [570.0, 29.0],
+                [600.0,100.0],
+                [120.0, 200.0],
+                [33.0, 200.0],
+                [-70.0, 171.0],
+                [-100.0, 100.0]
+                ])
+
+def load_path(file_path):
+    xs = []
+    ys = []
+    with open(file_path, "r") as f:
+        for i, line in enumerate(f, start=1):
+            line = line.strip()
+            if not line or line.startswith('#'):  # Salta righe vuote o commenti
+                continue
+                
+            parts = [p.strip() for p in line.split(',')]
+            if len(parts) != 2:
+                print(f"Riga {i} malformata: {repr(line)}")
+                continue
+                
+            try:
+                xs.append(float(parts[0]))
+                ys.append(float(parts[1]))
+            except ValueError as e:
+                print(f"Riga {i} non numerica: {parts} -> {e}")
+                continue
+                
+    print(f"Caricati {len(xs)} punti validi")
+    return xs, ys
+
+# Load path and create a spline
+xs, ys = load_path("oval_trj.txt")
+path_spline = cubic_spline_planner.Spline2D(xs, ys)
+
+def point_transform(trg, pose, yaw):
+
+    local_trg = [trg[0] - pose[0], trg[1] - pose[1]]
+
+    return local_trg
+
+def lateral_error_calc(trg, pose, yaw):
+    dx = trg[0] - pose[0]
+    dy = trg[1] - pose[1]
+
+    cos_y = math.cos(yaw)
+    sin_y = math.sin(yaw)
+
+    x_local =  cos_y * dx + sin_y * dy
+    y_local = -sin_y * dx + cos_y * dy
+
+    return [x_local, y_local]
+
+def plot_comparison(results, labels, title, xlabel, ylabel, settling_timestep = None):
+    """ Plot comparison of results for a specific state variable. """
+    plt.figure(figsize=(10, 6))
+    for i, result in enumerate(results):
+        plt.plot(result, label=labels[i])
+
+    if settling_timestep is not None:
+        plt.axvline(x=settling_timestep, color='r', linestyle='--', label=f'Settling Time: {settling_timestep*dt:.2f} ms')
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("./img/" + title + ".png", dpi=300, bbox_inches='tight')
+
+def plot_trajectory(x_vals, y_vals, labels, path_spline, frenet_x_results, frenet_y_results):
+    """ Plot 2D trajectory (x vs y) for all simulation configurations and path_spline trajectory. """
+    plt.figure(figsize=(10, 6))
+    
+    # Plot the simulation trajectories
+    for i in range(len(x_vals)):
+        plt.plot(x_vals[i], y_vals[i], label=labels[i])
+
+    # Plot the frenet planner trajectory
+    for i in range(len(frenet_x_results)):
+        plt.plot(frenet_x_results[i], frenet_y_results[i], label="Frenet Spline", linestyle="--", color="green")
+
+    # Plot the path_spline trajectory
+    spline_x = [path_spline.calc_position(s)[0] for s in np.linspace(0, path_spline.s[-1], 1000)]
+    spline_y = [path_spline.calc_position(s)[1] for s in np.linspace(0, path_spline.s[-1], 1000)]
+    plt.plot(spline_x, spline_y, label="Path Spline", linestyle="--", color="red")
+
+    # Plot obstacles
+    if(len(ob[0]) is not 0):
+        plt.scatter(ob[:, 0], ob[:, 1], c='black', label="Obstacles", marker='x')
+    
+    # Customize plot
+    plt.title("2D Trajectory Comparison")
+    plt.xlabel("X Position (m)")
+    plt.ylabel("Y Position (m)")
+    plt.legend()
+    plt.grid(True)
+    plt.axis("equal")
+    plt.savefig("./img/Trajectory.png", dpi=300, bbox_inches='tight')
+    plt.show()
+
+# This function plots the Fy vs alpha graphs
+def plot_lateral_force(Fy, alpha, labels, force_type):
+    """ Plot lateral force as a function of slip angle """
+    plt.figure(figsize=(10, 6))
+    for i, simulation in enumerate(Fy):
+        plt.plot(alpha[i], Fy[i], label=labels[i])
+    plt.title(force_type + " lateral force as function of slip angle")
+    plt.xlabel(force_type + " lateral slip angle (rad)")
+    plt.ylabel(force_type + " lateral force (N)")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("./img/" + force_type + " Lateral Force vs Slip Angle.png", dpi=300, bbox_inches='tight')
+
+def run_simulation(ax, steer, dt, integrator, model, steps=500):
+    """ Run a simulation with the given parameters and return all states. """
+
+    # Initialize the simulation
+    sim = Simulation(lf, lr, mass, Iz, dt, integrator=integrator, model=model)
+
+    # Storage for state variables and slip angles    
+    results = {
+        "x": [],
+        "y": [],
+        "theta": [],
+        "vx": [],
+        "vy": [],
+        "r": [], # yaw rate
+        "alpha_front": [], # front tires slip angle
+        "alpha_rear": [], # rear tires slip angle
+        "steer": [], # steering angle (delta)
+        "beta": [], # side slip angle
+        "Fy_front": [], # front tires lateral force
+        "Fy_rear": [], # rear tires lateral force
+        "ax": [], # acceleration
+        "velocity_error": [], # error w.r.t. target speed
+        "lateral_error": [], # position error w.r.t. path
+        "frenet_x": [], # next x position of the planned path
+        "frenet_y": [], # next y position of the planned path
+    }
+
+    casadi_model()
+
+    # Settling time variables
+    settling_treshold = target_speed * 0.05
+    settling_timestep = 0
+    settled = False
+
+    # states for Frenet-planner
+    c_speed = 0.0  # current speed [m/s]
+    c_accel = 0.0  # current acceleration [m/ss]
+    c_d = 0.0  # current lateral position [m]
+    c_d_d = 0.0  # current lateral speed [m/s]
+    c_d_dd = 0.0  # current lateral acceleration [m/s]
+    s0 = 0.0  # current course position
+
+    for step in range(steps):
+    
+        # Print time
+        print("Time:", step*dt)
+
+        # Calculate ax to track speed
+        ax, velocity_error = long_control_pid.compute(target_speed, sim.vx, dt)
+
+        # This mechanism allows to keep track of the settling time
+        if not settled:
+            if abs(velocity_error) <= settling_treshold:
+                settling_timestep = step
+                settled = True
+        else:
+            if abs(velocity_error) > settling_treshold:
+                settled = False
+
+        ### FRENET-PLANNER
+        frenet_path = fp.frenet_optimal_planning(
+            path_spline, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, ob)
+        
+        if(frenet_path is None):
+            print("None available paths found from Frenet...")
+            break
+
+        frenetpath_spline = cubic_spline_planner.Spline2D(frenet_path.x, frenet_path.y)
+
+        # The following lines compute the projection of the current vehicle position on the path. Keep in mind that
+        # the vehicle has a global position <x,y>. By the following code we are looking for the position of the vehicle position_projected
+        # on the path. "If the vehicle was following the path perfectly, where would it be now?"
+        actual_position = sim.x, sim.y # actual position of the vehicle in global coords
+        path_spline.update_current_s(actual_position) # update the cur_s longitudinal coord in the reference spline
+        frenetpath_spline.update_current_s(actual_position) # update the cur_s longitudinal coord in the planned spline
+
+        ## LATERAL ERROR CHECK
+        # Compute the distance between the actual position and the projection point in order to get the lateral error (Y coord)
+        
+        # get actual position projected on the path/spline
+        global_position_projected = path_spline.calc_position(path_spline.cur_s) # get the projection of the vehicle on the spline (in global coords)
+        prj = [ global_position_projected[0], global_position_projected[1] ] # this is the point where the vehicle would be on the path in the ideal case
+        local_error = lateral_error_calc(prj, actual_position, sim.theta)
+
+        if(abs(local_error[1]) > 4.0):
+            print("Lateral error is higher than 4.0... ending the simulation")
+            print("Lateral error: ", local_error[1])
+            break
+
+        local_position_projected = frenetpath_spline.calc_position(frenetpath_spline.cur_s)
+        prj = [ local_position_projected[0], local_position_projected[1] ]
+        frenetlocal_error = point_transform(prj, actual_position, sim.theta)
+
+        nearest_idx = 0
+        nearest_distance = abs(path_spline.cur_s - frenet_path.s[0])
+        for i in range(len(frenet_path.s)):
+            dist = abs(path_spline.cur_s - frenet_path.s[i])
+            if(dist < nearest_distance):
+                nearest_distance = dist
+                nearest_idx = i
+
+        s0 = frenet_path.s[nearest_idx]
+        c_d = frenet_path.d[nearest_idx]
+        c_d_d = frenet_path.d_d[nearest_idx]
+        c_d_dd = frenet_path.d_dd[nearest_idx]
+        c_speed = frenet_path.s_d[nearest_idx]
+        c_accel = frenet_path.s_dd[nearest_idx]
+
+        ### PURE PURSUIT
+
+        # The following lines compute the target position in global coords, based on the lookahead distance
+        # Bonus: include here the curvature dependency
+        Lf = k_pp * sim.vx + look_ahead # compute the lookahead distance, depending on the current velocity (the look_ahead variable is the minimum value of Lf)
+        # TO-DO: Extend it to depend on curvature and/or speed
+        s_pos = frenetpath_spline.cur_s + Lf # add calculated distance to the current longitudinal position
+        trg = frenetpath_spline.calc_position(s_pos) # get global target coords <x,y>
+
+        # Adjust CoG position to the rear axle position for PP
+        pp_position = actual_position[0] + lr * math.cos(sim.theta), actual_position[1] + lr * math.sin(sim.theta)
+
+        # Compute distance between vehicle rear axle and lookahead point, both expressed in global coords
+        target_pos_local = point_transform([trg[0], trg[1]], pp_position, sim.theta)
+
+        # Calculate steer to track path
+        steer = pp_controller.compute_steering_angle(target_pos_local, sim.theta, Lf)
+
+        # ### STANLEY
+
+        # # Adjust CoG position to the front axle position (convention for Stanley)
+        # px_front = position_projected[0] + lf * math.cos(sim.theta)
+        # py_front = position_projected[1] + lf * math.sin(sim.theta)
+        # stanley_target = px_front, py_front, frenetpath_spline.calc_yaw(frenetpath_spline.cur_s)
+        
+        # actual_pose = sim.x, sim.y, sim.theta
+        # steer = stanley_controller.compute_steering_angle(actual_pose, stanley_target, sim.vx)
+
+        # ### MPC
+
+        # # The following lines compute the target point on the path for the future horizon
+        # targets = [ ]
+        # s_pos = frenetpath_spline.cur_s
+        # for i in range(N):
+        #     step_increment = (sim.vx)*dt
+        #     trg = frenetpath_spline.calc_position(s_pos)
+        #     t_yaw = frenetpath_spline.calc_yaw(s_pos)
+        #     trg = [ trg[0], trg[1], t_yaw ]
+        #     targets.append(trg)
+        #     s_pos += step_increment
+
+        # steer = opt_step(targets, sim)
+        # Make one step simulation via model integration
+        sim.integrate(ax, float(steer))
+        
+        # Append each state to corresponding list
+        results["x"].append(sim.x)
+        results["y"].append(sim.y)
+        results["theta"].append(sim.theta)
+        results["vx"].append(sim.vx)
+        results["vy"].append(sim.vy)
+        results["r"].append(sim.r)
+
+        # Get other simulation values
+        results["alpha_front"].append(sim.alpha_front)
+        results["alpha_rear"].append(sim.alpha_rear)
+        results["steer"].append(float(steer))
+        results["beta"].append(sim.beta)
+        results["Fy_front"].append(sim.Fy_front)
+        results["Fy_rear"].append(sim.Fy_rear)
+        results["ax"].append(ax)
+
+        # Get Frenet path coords
+        results["frenet_x"].append(frenet_path.x[0])
+        results["frenet_y"].append(frenet_path.y[0])
+
+        # Errors
+        results["velocity_error"].append(velocity_error)
+        results["lateral_error"].append(local_error[1])
+
+    return results, settling_timestep
+
+def main():
+
+    # List of configurations
+    configs = [
+        ("rk4", "nonlinear"),
+    ]
+
+    # Run each simulation and store the results
+    all_results = []
+    labels = []
+    settling_timestep = []
+
+    # Each results is an array of subsequent states of a variable, for example in result[0] there will be the array corresponding
+    # to the X coord. Then, in all_results will be a matrix of arrays: one line for each simulation and one cell for each subsequent values
+    # of a state in that simulation.
+    for integrator, model in configs:
+        results, settling_timestep = run_simulation(ax, steer, dt, integrator, model, steps)
+        all_results.append(results)
+        labels.append(f"{integrator.capitalize()} - {model.capitalize()}")
+
+    # Separate each value for plotting
+    x_results = [result["x"] for result in all_results]
+    y_results = [result["y"] for result in all_results]
+    theta_results = [result["theta"] for result in all_results]
+    vx_results = [result["vx"] for result in all_results]
+    vy_results = [result["vy"] for result in all_results]
+    r_results = [result["r"] for result in all_results]
+    alpha_front_results = [result["alpha_front"] for result in all_results]
+    alpha_rear_results = [result["alpha_rear"] for result in all_results]
+    steer_results = [result["steer"] for result in all_results]
+    beta_results = [result["beta"] for result in all_results]
+    Fy_front_results = [result["Fy_front"] for result in all_results]
+    Fy_rear_results = [result["Fy_rear"] for result in all_results]
+    ax_results = [result["ax"] for result in all_results]
+    velocity_error_results = [result["velocity_error"] for result in all_results]
+    lateral_error_results = [result["lateral_error"] for result in all_results]
+    frenet_x_results = [result["frenet_x"] for result in all_results]
+    frenet_y_results = [result["frenet_y"] for result in all_results]
+
+    # Plot comparisons for each simulation value
+    plot_trajectory(x_results, y_results, labels, path_spline, frenet_x_results, frenet_y_results)
+    plot_comparison(theta_results, labels, "Heading Angle Comparison", "Time Step", "Heading Angle (rad)")
+    plot_comparison(vx_results, labels, "Longitudinal Velocity Comparison", "Time Step", "Velocity (m/s)", settling_timestep)
+    plot_comparison(vy_results, labels, "Lateral Velocity Comparison", "Time Step", "Lateral Velocity (m/s)")
+    plot_comparison(r_results, labels, "Yaw Rate Comparison", "Time Step", "Yaw Rate (rad/s)")
+    plot_comparison(alpha_front_results, labels, "Front Slip Angle Comparison", "Time Step", "Slip Angle (rad) - Front")
+    plot_comparison(alpha_rear_results, labels, "Rear Slip Angle Comparison", "Time Step", "Slip Angle (rad) - Rear")
+    plot_comparison(steer_results, labels, "Steering Angle Comparison", "Time Step", "Steering Angle (rad)")
+    plot_comparison(beta_results, labels, "Side Slip Angle Comparison", "Time Step", "Side Slip Angle (rad)")
+    plot_comparison(ax_results, labels, "Longitudinal Acceleration Comparison", "Time Step", "Acceleration (m/s^2)")
+    plot_comparison(velocity_error_results, labels, "Velocity Error", "Time Step", "Velocity Error (m/s)", settling_timestep)
+    plot_comparison(lateral_error_results, labels, "Lateral Error", "Time Step", "Lateral Error (m)")
+    plot_lateral_force(Fy_front_results, alpha_front_results, labels, "Front")
+    plot_lateral_force(Fy_rear_results, alpha_rear_results, labels, "Rear")
+
+if __name__ == "__main__":
+    main()
---
+++ assignment_06/simulation.py
@@ -0,0 +1,199 @@
+import numpy as np
+
+class Simulation:
+    def __init__(self, lf, lr, mass, Iz, dt, integrator="euler", model="kinematic"):
+        """
+        Initialize the simulation parameters.
+        """
+        self.l_f = lf                   # Distance to front axle (m)
+        self.l_r = lr                   # Distance to rear axle (m)
+        self.l_wb = lf + lr
+        self.mass = mass                # Vehicle mass (kg)
+        self.I_z = Iz                   # Yaw moment of inertia (kg*m^2)
+        self.dt = dt                    # Time step (s)
+        self.sinusoidal_steer = False    # Sinusoidal steer (True/False)
+        self.integrator = integrator    # Integrator choice
+        self.model = model              # Model choice
+        
+        # Aerodynamic and rolling resistance parameters
+        self.rho = 1.225               # Air density (kg/m^3)
+        self.C_d = 0.3                 # Drag coefficient (typical for cars)
+        self.A = 2.2                   # Frontal area (m^2)
+        self.C_rr = 0.015              # Rolling resistance coefficient
+
+        # Initialize states
+        self.x = 0                      # X position (m)
+        self.y = 0                      # Y position (m)
+        self.theta = 0                  # Heading angle (rad)
+        self.vx = 0                     # Longitudinal velocity (m/s)
+        self.vy = 0                     # Lateral velocity (m/s)
+        self.r = 0                      # Yaw rate (rad/s)
+
+        # Pacejka's Magic Formula coefficients
+        self.B, self.C, self.D, self.E = 7.1433, 1.3507, 1.0489, -0.0074722
+        self.B_front, self.C_front, self.D_front, self.E_front = self.B, self.C, self.D, self.E
+        self.B_rear, self.C_rear, self.D_rear, self.E_rear = self.B, self.C, self.D, self.E
+        
+        # Cornering stiffness front/rear (N/rad)
+        self.cornering_stiffness_front = self.B_front*self.C_front*self.D_front
+        self.cornering_stiffness_rear = self.B_rear*self.C_rear*self.D_rear  
+
+        # The following states are kept for plotting, keeping these values is not needed throughout the simulation
+        self.alpha_front = 0
+        self.alpha_rear = 0
+        self.Fy_front = 0
+        self.Fy_rear = 0
+        self.beta = 0
+
+    def kinematic_model(self, ax, delta):
+        """ Kinematic single-track model equations of motion. """
+        
+        # Total longitudinal force including aerodynamic drag and rolling resistance forces
+        F_aero = 0.5 * self.rho * self.C_d * self.A * (self.vx**2)
+        F_roll = self.C_rr * self.mass * 9.81
+        Fx = ax * self.mass - (F_aero + F_roll)
+
+        # Side slip angle
+        self.beta = np.arctan(self.vy / self.vx)
+        
+        # The array is in the format [dx, dy, dtheta, dvx, dvy, dr]
+        dx = np.array([
+            self.vx * np.cos(self.theta) - self.vy * np.sin(self.theta),    
+            self.vx * np.sin(self.theta) + self.vy * np.cos(self.theta),    
+            self.vx * np.tan(delta) / self.l_wb,                            
+            (1/self.mass) * Fx + self.r * self.vy,      
+            0,                                                              
+            0                                                               
+        ])
+
+        return dx
+
+    def linear_single_track_model(self, ax, delta):
+        """ Linear single-track model with aerodynamic and rolling resistance. """
+
+        # Total longitudinal force including aerodynamic drag and rolling resistance forces
+        F_aero = 0.5 * self.rho * self.C_d * self.A * (self.vx**2)
+        F_roll = self.C_rr * self.mass * 9.81
+        Fx = ax * self.mass - (F_aero + F_roll)
+
+        # Vertical forces (nominal vertical load)
+        F_n = self.mass * 9.81
+        Fz_front_nominal = (self.l_r / self.l_wb) * F_n
+        Fz_rear_nominal = (self.l_f / self.l_wb) * F_n
+
+        # Side slip angle
+        self.beta = np.arctan(self.vy / self.vx)
+
+        # Front and rear lateral forces computed based on the tire slip angles
+        # This is the linear part (valid just for small slip angles)
+        self.alpha_front = delta - (self.vy + self.l_f * self.r) / self.vx
+        self.alpha_rear = - (self.vy - self.l_r * self.r) / self.vx
+        self.Fy_front = Fz_front_nominal * self.cornering_stiffness_front * self.alpha_front
+        self.Fy_rear = Fz_rear_nominal * self.cornering_stiffness_rear * self.alpha_rear
+
+        # Dynamics equations
+        # The array is in the format [dx, dy, dtheta, dvx, dvy, dr]
+        dx = np.array([
+            self.vx * np.cos(self.theta) - self.vy * np.sin(self.theta),              
+            self.vx * np.sin(self.theta) + self.vy * np.cos(self.theta),               
+            self.r,                                                                    
+            (1/self.mass) * (Fx - self.Fy_front * np.sin(delta)) + self.vy * self.r,               
+            (1/self.mass) * (self.Fy_rear + self.Fy_front * np.cos(delta)) - self.r * self.vx,    
+            (1/self.I_z) * (self.Fy_front * self.l_f * np.cos(delta) - self.Fy_rear * self.l_r)   
+        ])
+        
+        return dx
+
+    def nonlinear_single_track_model(self, ax, delta):
+        """ Nonlinear single-track model with aerodynamic and rolling resistance. """
+
+        # Total longitudinal force including aerodynamic drag and rolling resistance forces
+        F_aero = 0.5 * self.rho * self.C_d * self.A * (self.vx**2)
+        F_roll = self.C_rr * self.mass * 9.81
+        Fx = ax * self.mass - (F_aero + F_roll)
+
+        # Vertical forces (nominal vertical load)
+        F_n = self.mass * 9.81
+        Fz_front_nominal = (self.l_r / self.l_wb) * F_n
+        Fz_rear_nominal = (self.l_f / self.l_wb) * F_n
+
+        # Side slip angle
+        # Please note: in order to make the model works for small velocities too, a minimum value of 0.5 has been set for the denominator
+        self.beta = np.arctan(self.vy / max(0.5,self.vx))
+
+        # Front and rear lateral forces computed based on the tire slip angles
+        # This is the non-linear part, used to represent lateral forces beyond the "small slip angle" region
+        # Please note: in order to make the model works for small velocities too, a minimum value of 0.5 has been set for the denominator
+        self.alpha_front = delta - np.arctan((self.vy + self.l_f * self.r) / max(0.5, self.vx))
+        self.alpha_rear = - np.arctan((self.vy - self.l_r * self.r) / max(0.5, self.vx))
+        self.Fy_front = Fz_front_nominal * self.D_front * np.sin(self.C_front * np.arctan(self.B_front * self.alpha_front - self.E_front * (self.B_front * self.alpha_front - np.arctan(self.B_front * self.alpha_front))))
+        self.Fy_rear = Fz_rear_nominal * self.D_rear * np.sin(self.C_rear * np.arctan(self.B_rear * self.alpha_rear - self.E_rear * (self.B_rear * self.alpha_rear - np.arctan(self.B_rear * self.alpha_rear))))
+
+        # Dynamics equations
+        # The array is in the format [dx, dy, dtheta, dvx, dvy, dr]
+        dx = np.array([
+        self.vx * np.cos(self.theta) - self.vy * np.sin(self.theta),    
+            self.vx * np.sin(self.theta) + self.vy * np.cos(self.theta),   
+            self.r,                                                                
+            (1/self.mass) * (Fx - self.Fy_front * np.sin(delta)) + self.vy * self.r,   
+            (1/self.mass) * (self.Fy_rear + self.Fy_front * np.cos(delta)) - self.r * self.vx, 
+            (1/self.I_z) * (self.Fy_front * self.l_f * np.cos(delta) - self.Fy_rear * self.l_r) 
+        ])
+        
+        return dx
+
+    def integrate(self, ax, delta):
+        """ Select the integrator method and apply it to update the state. """
+        if self.integrator == "euler":
+            self.euler_step(ax, delta)
+        elif self.integrator == "rk4":
+            self.rk4_step(ax, delta)
+
+    def euler_step(self, ax, delta):
+        """ Euler integration method. """
+        dx = self.compute_dx(ax, delta)
+        self.update_state(dx)
+
+    def rk4_step(self, ax, delta):
+        """ Runge-Kutta 4th order integration method. """
+        k1 = self.compute_dx(ax, delta)
+        self.update_state(k1, scale=0.5)
+        
+        k2 = self.compute_dx(ax, delta)
+        self.update_state(k2, scale=0.5, revert=k1, revert_scale=0.5)
+        
+        k3 = self.compute_dx(ax, delta)
+        self.update_state(k3, scale=1, revert=k2, revert_scale=0.5)
+
+        k4 = self.compute_dx(ax, delta)
+        self.update_state(np.zeros(6), scale=1, revert=k3, revert_scale=1)
+        
+        # Combine k1, k2, k3, k4 for RK4 update
+        dx = (k1 + 2*k2 + 2*k3 + k4) / 6
+        self.update_state(dx)
+
+    def compute_dx(self, ax, delta):
+        """ Compute the state derivatives using the chosen model. """
+        if self.model == "kinematic":
+            return self.kinematic_model(ax, delta)
+        elif self.model == "linear":
+            return self.linear_single_track_model(ax, delta)
+        elif self.model == "nonlinear":
+            return self.nonlinear_single_track_model(ax, delta)
+
+    def update_state(self, dx, scale=1, revert=None, revert_scale=1):
+        """ Update state with scaled dx. Optionally revert previous state for RK4. """
+        if revert is not None:
+            self.x -= revert[0] * self.dt * revert_scale
+            self.y -= revert[1] * self.dt * revert_scale
+            self.theta -= revert[2] * self.dt * revert_scale
+            self.vx -= revert[3] * self.dt * revert_scale
+            self.vy -= revert[4] * self.dt * revert_scale
+            self.r -= revert[5] * self.dt * revert_scale
+
+        self.x += dx[0] * self.dt * scale
+        self.y += dx[1] * self.dt * scale
+        self.theta += dx[2] * self.dt * scale
+        self.vx += dx[3] * self.dt * scale
+        self.vy += dx[4] * self.dt * scale
+        self.r += dx[5] * self.dt * scale
